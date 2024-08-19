00f1e0f243b9e5a978f9ef084b730797372c096a
renan m <renan.morita@etec.sp.gov.br>
2024-08-19 09:06:02 -0300

    spring oneegg

---
+++ meuprimeirospring/src/main/java/com/meuprimeirospring/meuprimeirospring/MeuprimeirospringApplication.java
@@ -0,0 +1,13 @@
+package com.meuprimeirospring.meuprimeirospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuprimeirospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprimeirospringApplication.class, args);
+	}
+
+}
---
+++ meuprimeirospring/src/test/java/com/meuprimeirospring/meuprimeirospring/MeuprimeirospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprimeirospring.meuprimeirospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuprimeirospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
